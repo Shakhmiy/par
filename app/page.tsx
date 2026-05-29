e77c66db7bfd0424d693eb62550164cce610c90e
Cybersport <Cybersport@26kadr.internal>
2026-05-29 12:34:33 +0300

    none footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,80 @@ import Link from "next/link";
 import Image from "next/image"; 
 import Logo from "@/public/Logo.svg";
 import Header from "@/components/layout/header";
-import Kos from "@/public/arrow-up-left.svg"
+import Kos from "@/public/sadasd.svg"
+import Arrow from "@/public/arrow-up-left.svg"
 import Card from "@/components/ui/card";
 import Gor from "@/public/Vecctor.svg";
+import Txt from "@/public/txt.svg"
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col text-center">
-      <Card/>
-      <Image className="connainer mx-auto"
+    <div className="flex min-h-screen flex-col text-center">
+     <div className=" h-screen flex flex-col justify-center text-center text-white gap-10 relative">
+        <h1 className="text-8xl">ПАРТНЁРСКИЕ ПРОЕКТ 26 КАДР</h1>
+
+        
+        <p className="text-4xl">Практико-ориентированное обучение, <br/>  подготовка специалистов и совместные <br/> проекты с индустрией</p>
+        <div className="flex justify-center">
+          <div className="flex justify-center items-center rounded-full bg-white w-[228] h-[48] gap-2 ">
+            <p className="text-black py-4 px-6">Смотреть кейсы</p>
+            <Image
+              src={Kos}
+              width={48}
+              height={48}
+              alt="a cool Image" 
+            />
+          </div>
+          </div>
+          <Image className=" absolute bottom-0 left-0 leading-none w-full"
               src={Gor}
               width={3000}
               height={200}
               alt="a cool Image" 
             />
-      <div className="bg-white min-h-screen">
-        <div className="container mx-auto flex flex-col w-[1160px] mt-8 gap-12"> 
-          <h3 className="text-black w-[320px] justify-start items-center font-light">Кейсы</h3>
+      </div>  
+      
+      <div className="bg-white">
+        <div className="flex container mx-auto flex-col gap-9 pt-18">
+          <h1 className="flex text-5xl justify-start text-black text-[55px]">Кейсы</h1>
+          <div className="grid grid-cols-3 gap-5">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+          <div className="flex container mx-auto flex-row justify-between my-16">
+            <div className="flex flex-col gap-5">
+              <h1 className="text-5xl">СТАНЬТЕ ПАРТНЁРОМ 26 КАДР</h1>
+              <p className="text-xl text-start">Обсудим совместные проекты, <br /> подготовку специалистов и форматы <br /> сотрудничества для вашего бизнеса</p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-start rounded-[8] bg-[#F2F2F2] w-[568] h-[53] gap-2">
+                <p className="text-black py-4 px-6 text-bace">ФИО</p>
+              </div>
+              <div className="flex justify-start rounded-[8] bg-[#F2F2F2] w-[568] h-[53] gap-2">
+                <p className="text-black py-4 px-6 text-bace">Email или телефон</p>
+              </div>
+              <div className="flex justify-start rounded-[8] bg-[#F2F2F2] w-[568] h-[53] gap-2">
+                <p className="text-black py-4 px-6 text-bace">Компания</p>
+              </div>
+              <div className="flex justify-center rounded-[8] bg-[#00d812] w-[568] h-[53] gap-2">
+                <p className="text-black py-4 px-6 text-bace">Отправить</p>
+              </div>
+              <div className="flex flex-row gap-2">
+                <Image className="" src={Txt} width={24} height={24} alt="a cool Image" />
+                <p className="text-xl">Согласен на обработку персональных данных</p>
+              </div>
+              
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
   )
 }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,25 +1,20 @@
 import Link from "next/link";
 import Image from "next/image"; 
 import Logo from "@/public/Logo.svg";
-import Сadr from "@/components/layout/header";
-import Kos from "@/public/arrow-up-left.svg"
+import Header from "@/components/layout/header";
+import Kos from "@/public/sadasd.svg"
+import Arrow from "@/public/arrow-up-left.svg"
+import Gor from "@/public/Vecctor.svg";
 
 export default function Card() {
-    return (
-        <div className="container mx-auto flex flex-col justify-center text-center text-white mt-60 gap-10">
-        <h1 className="text-8xl">ПАРТНЁРСКИЕ ПРОЕКТЫ 26 КАДРА</h1>
-        <p className="text-4xl">Практико-ориентированное обучение, <br/>  подготовка специалистов и совместные <br/> проекты с индустрией</p>
-        <div className="flex justify-center">
-          <div className="flex justify-center items-center rounded-[9999] bg-white w-[228] h-[48] gap-2 ">
-            <p className="text-black py-4 px-6">Смотреть кейсы</p>
-            <Image
-              src={Kos}
-              width={48}
-              height={48}
-              alt="a cool Image" 
-            />
+    return(
+          <div className="bg-[url(/fdfd.png)] bg-cover bg-no-repeat bg-center w-[480] h-[300] rounded-2xl">
+              <div className="flex flex-row p-4 pt-8 gap-35">
+                <p className="flex text-4xl text-start text-white">Отраслевые <br />  корпоративные <br /> чемпионаты</p>
+                <Image className="" src={Arrow} width={48} height={48} alt="a cool Image" />
+              </div>
+              <p className="text-sm text-start w-[336] h-[84] m-4 text-white">Оценка квалификации сотрудников, <br /> актуализация <br /> механизмов кадрового обеспечения <br /> предприятия</p>
           </div>
-          </div>
-      </div>
     )
 }
+
